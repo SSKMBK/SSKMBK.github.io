60de92a95ff54229eb758b7cba45d90124ccef0a
SSKMBK <60823190+SSKMBK@users.noreply.github.com>
2020-05-30 18:52:21 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -307,6 +307,7 @@ function deletebutton(){
     if(window.confirm("本当に保存データを削除しますか？")){
 
         localStorage.removeItem("salmonrandomweapondata_storage")
+        localStorage.removeItem("weapondata_storage")
         document.getElementById("save_weapondata").innerHTML="削除しました";
 
     }
